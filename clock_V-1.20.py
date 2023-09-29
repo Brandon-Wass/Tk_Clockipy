d0fc9f32bff9df68cf9c66272131742fc9186d1e
B-Boone <101531474+B-Boone@users.noreply.github.com>
2023-09-29 13:04:01 -0400

    Update clock_V-1.20.py

--- clock_V-1.20.py
+++ clock_V-1.20.py
@@ -8,16 +8,11 @@ import tkinter.messagebox as messagebox
 import RPi.GPIO as GPIO
 
 class Clock(tk.Tk):
+
     def __init__(self):
         super().__init__()
 
         self.setup_gpio()
-        
-    def setup_gpio(self):
-        GPIO.setwarnings(False)
-        GPIO.setmode(GPIO.BCM)
-        GPIO.setup(20, GPIO.OUT)
-        GPIO.output(20, GPIO.LOW)  # Make sure the pin is low when the program starts
 
         # Set window to fullscreen
         self.attributes('-fullscreen', True)
@@ -32,17 +27,39 @@ class Clock(tk.Tk):
         self.canvas = tk.Canvas(self, width=self.screen_width, height=self.screen_height, bg='black', highlightthickness=0, cursor="none")
         self.canvas.pack(fill=tk.BOTH, expand=1)
 
-        # Compute radius directly in the __init__ method
-        center_x, center_y = self.screen_width / 2, self.screen_height / 2
-        radius = min(center_x, center_y) - 50
+        # Draw static elements (like clock circle, marks, numbers)
+        self.draw_static_elements()
 
         # Close the program by clicking anywhere on the window
         self.canvas.bind("<Button-1>", lambda e: self.destroy())
 
-        
         # Initial alarm time to None (no alarm)
         self.alarm_time = None
+
+        self.update_clock()
+
+    def setup_gpio(self):
+        GPIO.setwarnings(False)
+        GPIO.setmode(GPIO.BCM)
+        GPIO.setup(20, GPIO.OUT)
+        GPIO.output(20, GPIO.LOW)  # Make sure the pin is low when the program starts
+
+    # Separate static elements drawing
+    def draw_static_elements(self):
+        center_x, center_y = self.screen_width / 2, self.screen_height / 2
+        radius = min(center_x, center_y) - 50
+
+        # Draw the clock circle
+        self.canvas.create_oval(center_x - radius, center_y - radius, center_x + radius, center_y + radius, fill="black", outline="purple")
         
+        # Draw static marks and numbers
+        self.draw_marks()
+        self.draw_numbers()
+
+        # Buttons and initial alarm display
+        self.draw_alarm_display_and_buttons()
+
+    def draw_alarm_display_and_buttons(self):
         # Display for alarm time
         self.alarm_display = tk.Label(self.canvas, text="--:--", font=('Arial', 20, 'bold'), bg="black", fg="purple")
         self.alarm_display.place(x=self.screen_width - 282, y=300, anchor="e")
@@ -58,13 +75,33 @@ class Clock(tk.Tk):
                                width=10, height=5, highlightbackground="purple", bd=4, relief=tk.FLAT)
         self.minute_button.place(x=self.screen_width, y=430, anchor="e")
 
-
-
-        self.update_clock()
-
-    def draw_hand(self, coord, color, width):
+    def update_clock(self):
         center_x, center_y = self.screen_width / 2, self.screen_height / 2
-        self.canvas.create_line(center_x, center_y, coord[0], coord[1], fill=color, width=width)
+        radius = min(center_x, center_y) - 50
+
+        current_time = time.localtime(time.time())
+
+        # Calculate angles for each hand
+        second_angle = math.radians(current_time.tm_sec * 6 - 90)
+        minute_angle = math.radians(current_time.tm_min * 6 - 90 + current_time.tm_sec * 0.1)
+        hour_angle = math.radians(current_time.tm_hour * 30 - 90 + current_time.tm_min * 0.5)
+
+        second_coords = (center_x + radius * math.cos(second_angle), center_y + radius * math.sin(second_angle))
+        minute_coords = (center_x + (radius - 30) * math.cos(minute_angle), center_y + (radius - 30) * math.sin(minute_angle))
+        hour_coords = (center_x + (radius - 60) * math.cos(hour_angle), center_y + (radius - 60) * math.sin(hour_angle))
+
+        # Delete and re-draw hands
+        self.canvas.delete("hand")  # We tag the hands with "hand" to delete them specifically
+        self.draw_hand(second_coords, "purple", int(radius/100), "hand")
+        self.draw_hand(minute_coords, "purple", int(radius/66), "hand")
+        self.draw_hand(hour_coords, "purple", int(radius/33), "hand")
+
+        self.check_alarm(current_time)
+        self.update_id = self.after(1000, self.update_clock)  # Update every 1 second for better performance
+
+    def draw_hand(self, coord, color, width, tag=None):
+        center_x, center_y = self.screen_width / 2, self.screen_height / 2
+        self.canvas.create_line(center_x, center_y, coord[0], coord[1], fill=color, width=width, tags=tag)
 
     def draw_marks(self):
         center_x, center_y = self.screen_width / 2, self.screen_height / 2
@@ -89,37 +126,6 @@ class Clock(tk.Tk):
             angle = math.radians(i * 30 - 90)
             coord = (center_x + (radius - 30) * math.cos(angle), center_y + (radius - 30) * math.sin(angle))
             self.canvas.create_text(coord[0], coord[1], text=str(i), font=('Arial', int(radius/10), 'bold'), fill="purple")
-
-    def update_clock(self):
-        self.canvas.delete("all")
-
-        center_x, center_y = self.screen_width / 2, self.screen_height / 2
-        radius = min(center_x, center_y) - 50
-
-        # Draw the clock circle
-        self.canvas.create_oval(center_x - radius, center_y - radius, center_x + radius, center_y + radius, fill="black", outline="purple")
-
-        self.draw_marks()
-        self.draw_numbers()
-
-        current_time = time.localtime(time.time())
-
-        # Calculate angles for each hand
-        second_angle = math.radians(current_time.tm_sec * 6 - 90)
-        minute_angle = math.radians(current_time.tm_min * 6 - 90 + current_time.tm_sec * 0.1)
-        hour_angle = math.radians(current_time.tm_hour * 30 - 90 + current_time.tm_min * 0.5)
-
-        second_coords = (center_x + radius * math.cos(second_angle), center_y + radius * math.sin(second_angle))
-        minute_coords = (center_x + (radius - 30) * math.cos(minute_angle), center_y + (radius - 30) * math.sin(minute_angle))
-        hour_coords = (center_x + (radius - 60) * math.cos(hour_angle), center_y + (radius - 60) * math.sin(hour_angle))
-
-        # Draw hands
-        self.draw_hand(second_coords, "purple", int(radius/100))
-        self.draw_hand(minute_coords, "purple", int(radius/66))
-        self.draw_hand(hour_coords, "purple", int(radius/33))
-
-        self.check_alarm(current_time)
-        self.update_id = self.after(10, self.update_clock)  # Update every 1 second
 
     def set_alarm_hour(self):
         if self.alarm_time is None:
@@ -153,13 +159,13 @@ class Clock(tk.Tk):
         response = messagebox.showinfo("Alarm", "Time's up!")
         if response:
             GPIO.output(20, GPIO.LOW)  # Set the GPIO pin to low when the dialog popup is closed
-        pass
 
-def cleanup_on_exit(self, event):
-    GPIO.cleanup()  # Cleanup GPIO configurations
-    self.setup_gpio()  # Reset the GPIO setup
-    self.after_cancel(self.update_id)
+    def cleanup_on_exit(self):
+        GPIO.cleanup()  # Clean up GPIO settings
+        self.after_cancel(self.update_id)
+        self.destroy()  # destroy the window
 
 if __name__ == "__main__":
     app = Clock()
+    app.protocol("WM_DELETE_WINDOW", app.cleanup_on_exit)
     app.mainloop()
